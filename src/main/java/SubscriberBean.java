d85652a3e6607f7405efd68bb71a19ad86b7cc44
Nikola Koevski <koevskin@gmail.com>
2017-09-15 12:05:34 +0200

    Add CSV library and a JavaBean

--- src/main/java/SubscriberBean.java
+++ src/main/java/SubscriberBean.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import com.opencsv.bean.CsvBindByName;
 
 public class SubscriberBean {
+
+    @CsvBindByName
+    private String Name;
+
+    @CsvBindByName
+    private String Email;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "SubscriberBean{" + "Name=" + Name + ", Email=" + Email + '}';
+    }
 }
